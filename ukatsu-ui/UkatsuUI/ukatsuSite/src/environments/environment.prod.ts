dcc9ef9a239ac6ada1b4f8a85d943e309e5511a7
Shendez <tiit37@mail.missouri.edu>
2018-12-13 19:58:55 -0600

    Update environment.prod.ts

--- ukatsu-ui/UkatsuUI/ukatsuSite/src/environments/environment.prod.ts
+++ ukatsu-ui/UkatsuUI/ukatsuSite/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCe6y4pGvitHVyl1gWjWV5lFWMF5mwyrGI',
+    apiKey: '(ApiKey)',
     authDomain: 'ukatsu-d0629.firebaseapp.com',
     databaseURL: 'https://ukatsu-d0629.firebaseio.com',
     projectId: 'ukatsu-d0629',
