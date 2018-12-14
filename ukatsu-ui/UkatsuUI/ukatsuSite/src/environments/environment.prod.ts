a8b9e9743125259518b9dcae1abc3b5eb4cb151e
Teddy <teddyivanov95@gmail.com>
2018-12-13 21:57:51 -0600

    Added key back

--- ukatsu-ui/UkatsuUI/ukatsuSite/src/environments/environment.prod.ts
+++ ukatsu-ui/UkatsuUI/ukatsuSite/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '(ApiKey)',
+    apiKey: 'AIzaSyCe6y4pGvitHVyl1gWjWV5lFWMF5mwyrGI',
     authDomain: 'ukatsu-d0629.firebaseapp.com',
     databaseURL: 'https://ukatsu-d0629.firebaseio.com',
     projectId: 'ukatsu-d0629',
